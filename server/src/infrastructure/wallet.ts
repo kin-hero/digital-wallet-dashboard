7370f68ab2273ec147e6d3dff712fe5165b3eb0f
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-19 15:16:31 +0900

    chore: just fix code line

--- server/src/infrastructure/wallet.ts
+++ server/src/infrastructure/wallet.ts
@@ -4,8 +4,7 @@ import { EtherscanTxResponseSchema } from "../schemas/etherscan.schema";
 
 export const getTheMostRecentTransactionOfAddress = async (address: EthereumAddress): Promise<EtherscanTxResponse> => {
   const apiKey = process.env.ETHERSCAN_API_KEY;
-  const url = `https://api.etherscan.io/v2/api?chainid=1&module=account&action=txlist&address=${address}&page=1&offset=1&sort=desc&apikey=${apiKey}
-`;
+  const url = `https://api.etherscan.io/v2/api?chainid=1&module=account&action=txlist&address=${address}&page=1&offset=1&sort=desc&apikey=${apiKey}`;
   const response = await fetch(url);
   const rawData = await response.json();
   const data: EtherscanTxResponse = EtherscanTxResponseSchema.parse(rawData);
