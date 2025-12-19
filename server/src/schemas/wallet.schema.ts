f18502ca9ec69a8db44107224da0f592babc344e
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-19 11:43:56 +0900

    feat: create check wallet schema

---
+++ server/src/schemas/wallet.schema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { ethereumAddressSchema, errorResponseSchema, successResponseSchema } from "./common.schema";
+
+export const checkWalletAgeParamsSchema = z.object({
+  address: ethereumAddressSchema,
+});
+
+export const checkWalletAgeResponseSchema = successResponseSchema.extend({
+  result: z.object({
+    isOld: z.boolean(),
+    lastTransactionDate: z.iso.date(),
+  }),
+});
+
+export const checkWalletAgeSchema = {
+  params: checkWalletAgeParamsSchema,
+  response: {
+    200: checkWalletAgeResponseSchema,
+    400: errorResponseSchema,
+    404: errorResponseSchema,
+    500: errorResponseSchema,
+  },
+};
+
+export type CheckWalletAgeParams = z.infer<typeof checkWalletAgeParamsSchema>;
+export type CheckWalletAgeResponse = z.infer<typeof checkWalletAgeResponseSchema>;
