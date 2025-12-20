cbd72859411d456e7cad1da3ef24d8230fb764db
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-20 14:15:53 +0900

    config: set up vitest

---
+++ server/vitest.setup.ts
@@ -0,0 +1,5 @@
+// Mock environment variables for tests
+process.env.ETHERSCAN_API_KEY = "test-api-key";
+process.env.PORT = "3001";
+process.env.NODE_ENV = "test";
+process.env.VALID_WALLET_YEARS = "1";
