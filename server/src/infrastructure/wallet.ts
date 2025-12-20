37c0b7ea89e2e1621e367e17354649bbf02810b2
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-20 13:15:37 +0900

    feat: update infrastructure with wallet balance with currency

--- server/src/infrastructure/wallet.ts
+++ server/src/infrastructure/wallet.ts
@@ -1,9 +1,9 @@
 import type { EthereumAddress } from "../schemas/common.schema";
-import type { EtherscanTxResponse } from "../schemas/etherscan.schema";
-import { EtherscanTxResponseSchema } from "../schemas/etherscan.schema";
+import type { EtherscanTxResponse, EtherscanBalanceResponse } from "../schemas/etherscan.schema";
+import { EtherscanTxResponseSchema, EtherscanBalanceResponseSchema } from "../schemas/etherscan.schema";
 import { config } from "../config";
 
-const getTheMostRecentTransactionOfAddress = async (address: EthereumAddress): Promise<EtherscanTxResponse> => {
+export const getTheMostRecentTransactionOfAddress = async (address: EthereumAddress): Promise<EtherscanTxResponse> => {
   const url = `https://api.etherscan.io/v2/api?chainid=1&module=account&action=txlist&address=${address}&page=1&offset=1&sort=desc&apikey=${config.etherscanApiKey}`;
   const response = await fetch(url);
   const rawData = await response.json();
@@ -11,4 +11,10 @@ const getTheMostRecentTransactionOfAddress = async (address: EthereumAddress): P
   return data;
 };
 
-export default getTheMostRecentTransactionOfAddress;
+export const getCurrentEthereumBalanceOfAddress = async (address: EthereumAddress): Promise<EtherscanBalanceResponse> => {
+  const url = `https://api.etherscan.io/v2/api?chainid=1&module=account&action=balance&address=${address}&apikey=${config.etherscanApiKey}`;
+  const response = await fetch(url);
+  const rawData = await response.json();
+  const data: EtherscanBalanceResponse = EtherscanBalanceResponseSchema.parse(rawData);
+  return data;
+};
