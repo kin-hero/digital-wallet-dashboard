8573d0eae054ca22a7263fc70c7db120fcd800a4
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-20 13:15:23 +0900

    feat: update schema with wallet balance with currency

--- server/src/schemas/wallet.schema.ts
+++ server/src/schemas/wallet.schema.ts
@@ -1,21 +1,26 @@
 import { z } from "zod";
-import { ethereumAddressSchema, errorResponseSchema, successResponseSchema } from "./common.schema";
+import { ethereumAddressSchema, errorResponseSchema, successResponseSchema, currencySchema } from "./common.schema";
 
-export const checkWalletAgeParamsSchema = z.object({
+export const ethereumAddressParams = z.object({
   address: ethereumAddressSchema,
 });
+export type EthereumAddressParams = z.infer<typeof ethereumAddressParams>;
+export const currencyQueryString = z.object({
+  currency: currencySchema,
+});
+export type CurrencyQueryString = z.infer<typeof currencyQueryString>;
 
 export const lastTransactionDateSchema = z.union([z.iso.date(), z.literal("Never")]);
-
 export const checkWalletAgeResponseSchema = successResponseSchema.extend({
   result: z.object({
     isOld: z.boolean(),
     lastTransactionDate: lastTransactionDateSchema,
   }),
 });
+export type CheckWalletAgeResponse = z.infer<typeof checkWalletAgeResponseSchema>;
 
 export const checkWalletAgeSchema = {
-  params: checkWalletAgeParamsSchema,
+  params: ethereumAddressParams,
   response: {
     200: checkWalletAgeResponseSchema,
     400: errorResponseSchema,
@@ -25,5 +30,25 @@ export const checkWalletAgeSchema = {
   },
 } as const;
 
-export type CheckWalletAgeParams = z.infer<typeof checkWalletAgeParamsSchema>;
-export type CheckWalletAgeResponse = z.infer<typeof checkWalletAgeResponseSchema>;
+export const walletBalanceWithCurrencyResponseSchema = successResponseSchema.extend({
+  result: z.object({
+    address: ethereumAddressSchema,
+    ethBalance: z.string(),
+    convertedAmount: z.number(),
+    currency: currencySchema,
+  }),
+});
+
+export type WalletBalanceWithCurrencyResponse = z.infer<typeof walletBalanceWithCurrencyResponseSchema>;
+
+export const walletBalanceWithCurrencySchema = {
+  querystring: currencyQueryString,
+  params: ethereumAddressParams,
+  response: {
+    200: walletBalanceWithCurrencyResponseSchema,
+    400: errorResponseSchema,
+    500: errorResponseSchema,
+    502: errorResponseSchema,
+    503: errorResponseSchema,
+  },
+} as const;
