24f0e2e6d3abb9b05429826faa64e52289c0b059
Keane Putra Setiawan <keanesetiawan.21@gmail.com>
2025-12-20 16:19:55 +0900

    feat: include dashboard content in the home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { DashboardContent } from "@/components/DashboardContent";
 import { ExchangeRateDisplay } from "@/components/ExchangeRateDisplay";
 import { ExchangeRateLoading } from "@/components/ExchangeRateLoading";
 
@@ -9,9 +10,11 @@ export default function Home() {
     <div className="flex min-h-screen flex-col bg-gray-50">
       <Header />
       <main className="container mx-auto flex-1 px-6 py-8">
-        <Suspense fallback={<ExchangeRateLoading />}>
-          <ExchangeRateDisplay />
-        </Suspense>
+        <DashboardContent>
+          <Suspense fallback={<ExchangeRateLoading />}>
+            <ExchangeRateDisplay />
+          </Suspense>
+        </DashboardContent>
       </main>
       <Footer />
     </div>
